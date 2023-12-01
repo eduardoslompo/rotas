0d564c9ce663619858734d1e25d8097eb37d504f
Eduardo <eduardo5slompo@gmail.com>
2023-11-30 22:07:14 -0300

    first commit

---
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { CursosService } from '../cursos/cursos.service';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent {
+
+  id!: number;
+  inscricao!: Subscription;
+  curso: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cursoService: CursosService){
+    // console.log(this.route);
+    // this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+
+        this.curso = this.cursoService.getCurso(this.id);
+
+        if(this.curso == null){
+          this.router.navigate(['/naoEncontrado']);
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+}
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CursosService } from './cursos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent {
+
+  cursos!:any[];
+  pagina!: number;
+  inscricao!: Subscription;
+
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    this.cursos = this.cursosService.getCursos();
+
+    this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    // this.pagina++;
+    this.router.navigate(['/cursos'], 
+  {queryParams: {'pagina': ++this.pagina}});
+  }
+
+}

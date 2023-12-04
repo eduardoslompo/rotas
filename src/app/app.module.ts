480ca463bed7a75f50bd97bd26c2a4163197fba3
Eduardo <eduardo5slompo@gmail.com>
2023-12-04 11:50:04 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,32 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/cursos.module';
+// import { CursosComponent } from './cursos/cursos.component';
 
 // import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     // routing,
+    CursosModule,
     AppRoutingModule
   ],
-  providers: [CursosService],
+  providers: [
+    // CursosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
